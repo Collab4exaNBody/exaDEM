acb0219b14db403050c7f01faa3706b58ff3f592
rprat-pro <rp269144@pleiades1217.intra.cea.fr>
2024-02-18 23:06:37 +0100

    add doxygen code for mgrid_cell_interaction.hpp

--- src/polyhedra/include/exaDEM/interaction/grid_cell_interaction.hpp
+++ src/polyhedra/include/exaDEM/interaction/grid_cell_interaction.hpp
@@ -10,13 +10,28 @@
  ***********************************************************************************/
 namespace exaDEM
 {
+	/**
+	 * @brief Struct representing interactions in DEM simulation.
+	 * It contains a memory-managed vector storing extra dynamic data storage for interactions.
+	 */
 	struct GridCellParticleInteraction
 	{
 		onika::memory::CudaMMVector< CellExtraDynamicDataStorageT<Interaction> > m_data;
 		GridCellParticleInteraction() {};
 	};
 
+	/**
+	 * @brief Alias for the migration helper for interactions.
+	 */
 	typedef ExtraDynamicDataStorageMigrationHelper<Interaction> InteractionMigrationHelper;
+
+	/**
+	 * @brief Alias for the cell move buffer for interactions.
+	 */
 	typedef ExtraDynamicDataStorageCellMoveBufferT<Interaction> InteractionCellMoveBuffer;
+
+	/**
+	 * @brief Alias for the grid move buffer for interactions.
+	 */
 	typedef ExtraDynamicStorageDataGridMoveBufferT<Interaction> InteractionGridMoveBuffer;
 }
