c7c2ea2da08daa782616370712867b8ff0a599cf
rprat-pro <rp269144@pleiades1217.intra.cea.fr>
2024-02-18 23:04:10 +0100

    add doxygen code for migration_helper.hpp

--- src/polyhedra/include/exaDEM/interaction/migration_helper.hpp
+++ src/polyhedra/include/exaDEM/interaction/migration_helper.hpp
@@ -5,73 +5,110 @@
 
 namespace exaDEM
 {
+	/**
+	 * @brief Template struct representing a migration helper for extra dynamic data storage.
+	 * This template struct serves as a migration helper for extra dynamic data storage.
+	 * It contains references to the cell extra data and an outside-the-box (OTB) buffer for migrating data.
+	 * @tparam T The type of extra dynamic data stored in the cell.
+	 */
 	template<typename T> struct ExtraDynamicDataStorageMigrationHelper
 	{
 		using uint = uint64_t;
 		using info = std::tuple<uint, uint, uint>;
 
 		// members
-		onika::memory::CudaMMVector< CellExtraDynamicDataStorageT<T> > & m_cell_extra_data;
-		ExtraDynamicDataStorageCellMoveBufferT<T> & m_otb_buffer;
-
-		// default constructor
-		//ExtraDynamicDataStorageMigrationHelper() {}
+		onika::memory::CudaMMVector< CellExtraDynamicDataStorageT<T> > & m_cell_extra_data; /**< Reference to the cell extra data. */
+		ExtraDynamicDataStorageCellMoveBufferT<T> & m_otb_buffer; /**< Reference to OTB buffer for migrating data. */
 
 		inline const unsigned int cell_particles_data_size ( size_t cell_i )
 		{
 			return m_cell_extra_data[ cell_i ]. storage_size();
 		}
 
-		//TODO FIX IT
+		/**
+		 * @brief Writes cell particles data into a buffer.
+		 * Then, it calls the 'encode_cell_to_buffer' function of the corresponding cell extra data to encode the data into the buffer.
+		 * @param buffer A pointer to the buffer where the cell particles data will be written.
+		 * @param cell_i The index of the cell whose particles data is to be written into the buffer.
+		 */
 		inline void write_cell_particles_data_in_buffer(void* buffer, size_t cell_i) const
 		{
-			/*
-			if( m_cell_extra_data.empty() ) 
-			{
-				m_cell_extra_data[ cell_i ].clear();
-			}*/
+			assert ( cell_i < m_cell_extra_data.size() );
 			m_cell_extra_data[ cell_i ].encode_cell_to_buffer(buffer);
 		}
 
+		/**
+		 * @brief Swaps particles in the outside-the-box (OTB) buffer.
+		 * @param a The index of the first particle to swap.
+		 * @param b The index of the second particle to swap.
+		 * @see ExtraDynamicDataStorageCellMoveBufferT::swap
+		 */
 		inline void swap_otb_particles( size_t a, size_t b )
 		{
 			m_otb_buffer.swap(a, b);
 		}
 
+		/**
+		 * @brief Calculates the storage size for an outside-the-box (OTB) range.
+		 * This function calculates the storage size required to accommodate an outside-the-box (OTB) range of particles.
+		 * @param pstart The start index of the OTB range.
+		 * @param pend The end index of the OTB range.
+		 * @return The storage size required for the OTB range.
+		 */
 		inline size_t storage_size_for_otb_range(size_t pstart, size_t pend)
 		{
+			assert ( pend >= pstart );  
 			const size_t n_particles = pend - pstart;
 			uint total_size = 0;
+
+			// Since particles can be reordered, 
+			// we have to iterate through the data to determine the item size.
 			for(size_t p = pstart ; p < pend ; p++)
 			{
-				auto sz = m_otb_buffer.particle_number_of_items(p);
+				uint sz = m_otb_buffer.particle_number_of_items(p);
 				total_size += sz * sizeof(T);
 			}
 
+			// Add global information and 'info' vector 
 			total_size += n_particles * sizeof(info) + 2 * sizeof(uint);
 			return total_size;
 		}
 
+		/**
+		 * @brief Serializes an outside-the-box (OTB) range into a buffer.
+		 * This function serializes the particles within the specified outside-the-box (OTB) range into a buffer.
+		 * @param to_buff A pointer to the buffer where the serialized data will be written.
+		 * @param pstart The start index of the OTB range.
+		 * @param pend The end index of the OTB range.
+		 * @return The number of bytes written into the buffer.
+		 */
 		inline size_t serialize_otb_range( void* to_buff, size_t pstart, size_t pend )
 		{
 			assert (m_otb_buffer.check_info_consistency() );
 			const size_t n_particles = pend - pstart;
 			const auto [from_glob_ptr, from_info_ptr, from_data_ptr] = m_otb_buffer.decode_pointers(n_particles);
-			// decode stream buffer pointers
+			// Decode stream buffer pointers.
 			uint* to_glob_ptr = (uint*) to_buff; // global information
 			info* to_info_ptr = (info*) (to_glob_ptr + 2);
 			T*    to_data_ptr = (T*) (to_info_ptr + n_particles);
-			to_glob_ptr[0] = n_particles;
-			to_glob_ptr[1] = 0;
+
+			// Set global information, number of items will be updated after
+			to_glob_ptr[0] = n_particles; 
+			to_glob_ptr[1] = 0; // number of items
 
 			uint total_size = n_particles * sizeof(info) + 2 * sizeof(uint); // total_size count in Word units
+			// We need to set the correct offset in the buffer, it is not store in the OTB vector
 			uint to_offset = 0;
+
+			// Iterate over the OTB vector to add 'info' + extra data storage 
 			for( size_t p = pstart ; p < pend ; p++)
 			{
 				const auto [from_offset, from_size, from_id] = m_otb_buffer.get_info(p); 
 				to_glob_ptr[1] += from_size;
-				total_size += from_size * sizeof(T);	
+				total_size += from_size * sizeof(T);
+				// update info	
 				to_info_ptr[p] = {to_offset, from_size, from_id}; // fit offset
+				// update data
 				std::copy ( from_data_ptr + from_offset, from_data_ptr + from_offset + from_size, to_data_ptr + to_offset);
 				to_offset += from_size;	
 			}
@@ -94,7 +131,25 @@ namespace exaDEM
 			}
 		}
 
+		// Default behavior used by the following function
 		struct NullParticleIdFunc { inline constexpr uint64_t operator () (size_t) { return 0; } };
+
+		/**
+		 * @brief Appends data to the stream using specified functions for cell indexing, locking, and unlocking.
+		 *
+		 * @tparam CellIndexFuncT The type of the function object for cell indexing.
+		 * @tparam CellLockFuncT The type of the function object for cell locking.
+		 * @tparam CellUnlockFuncT The type of the function object for cell unlocking.
+		 * @tparam ParticleIdFuncT The type of the function object for particle ID. Default is NullParticleIdFunc.
+		 *
+		 * @param datav The data stream.
+		 * @param data_bytes The size of the data to be appended.
+		 * @param part_seq_len The length of the particle sequence.
+		 * @param cell_index_func The function object for cell indexing.
+		 * @param cell_lock_func The function object for cell locking.
+		 * @param cell_unlock_func The function object for cell unlocking.
+		 * @param particle_id_func The function object for particle ID. Default is NullParticleIdFunc.
+		 */
 		template<class CellIndexFuncT, class CellLockFuncT, class CellUnlockFuncT, class ParticleIdFuncT = NullParticleIdFunc >
 			inline void append_data_stream(const void* datav, size_t data_bytes, size_t part_seq_len, CellIndexFuncT cell_index_func, CellLockFuncT cell_lock_func, CellUnlockFuncT cell_unlock_func, ParticleIdFuncT particle_id_func = {} )
 			{
