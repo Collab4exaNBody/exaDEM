136ef4c53d2718d2085652b1b8298aa5a7e76595
rprat-pro <rp269144@pleiades1217.intra.cea.fr>
2024-05-20 18:10:26 +0200

    add init_rsa operator

--- src/rsa/init_rsa.cpp
+++ src/rsa/init_rsa.cpp
@@ -16,6 +16,18 @@
 #include <exanb/core/file_utils.h>
 #include <exanb/core/domain.h>
 #include <exanb/core/check_particles_inside_cell.h>
+#include <exanb/core/simple_block_rcb.h>
+
+
+// rsa_mpi stuff
+#include <rsa_data_storage.hxx>
+#include <rsa_random.hxx>
+#include <rsa_domain.hxx>
+#include <rsa_decoration.hxx>
+#include <operator_algorithm.hxx>
+#include <radius_generator.hxx>
+
+
 
 namespace exaDEM
 {
@@ -49,14 +61,17 @@ using namespace exanb;
 		ADD_SLOT( double          , enlarge_bounds, INPUT , 0.0 );
 		ADD_SLOT( std::vector<bool> , periodicity     , INPUT ,OPTIONAL , DocString{"if set, overrides domain's periodicity stored in file with this value"}  );
 		ADD_SLOT( bool        , expandable      , INPUT ,OPTIONAL , DocString{"if set, override domain expandability stored in file"} );
-		ADD_SLOT( AABB        , bounds          , INPUT ,OPTIONAL , DocString{"if set, override domain's bounds, filtering out particle outside of overriden bounds"} );
-		ADD_SLOT( ParticleTypes   , particle_types , INPUT ); // optional. if no species given, type ids are allocated automatically
+		ADD_SLOT( AABB        , bounds          , INPUT ,REQUIRED , DocString{"if set, override domain's bounds, filtering out particle outside of overriden bounds"} );
+		ADD_SLOT( int         , type , INPUT , 0 );
+    ADD_SLOT( bool        , pbc_adjust_xform , INPUT , true );
+
+		ADD_SLOT( double   , radius , INPUT , REQUIRED ); // optional. if no species given, type ids are allocated automatically
 
 		public:
 		inline void execute () override final
 		{
 			//-------------------------------------------------------------------------------------------
-			using ParticleTupleIO = onika::soatl::FieldTuple<field::_rx, field::_ry, field::_rz, field::_id, field::_shape>;
+			using ParticleTupleIO = onika::soatl::FieldTuple<field::_rx, field::_ry, field::_rz, field::_id, field::_type>;
 			using ParticleTuple = decltype( grid->cells()[0][0] );
 
 			assert( grid->number_of_particles() == 0 );
@@ -65,77 +80,70 @@ using namespace exanb;
 			int rank=0, np=1;
 			MPI_Comm_rank(*mpi, &rank);
 			MPI_Comm_size(*mpi, &np);
-			uint64_t n_particles = 0;
+			[[maybe_unused]] uint64_t n_particles = 0;
 
-			std::map<std::string,unsigned int> typeMap;
-			unsigned int nextTypeId = 0;
-			if( particle_types.has_value() )
+			AABB b = *bounds;
+      constexpr int DIM = 3;
+			constexpr int method = 1;
+      constexpr int ghost_layer = 1;
+      std::array<double, DIM> domain_inf = {b.bmin.x, b.bmin.y, b.bmin.z};
+      std::array<double, DIM> domain_sup = {b.bmax.x, b.bmax.y, b.bmax.z};
+      rsa_domain<DIM> rsa_domain(domain_inf, domain_sup, ghost_layer, *radius);
+
+      //rsa_domain.domain_log();
+      size_t seed = 0;
+      algorithm::uniform_generate<DIM, method>(rsa_domain, *radius, 6000, 10, seed);
+      auto& cells = rsa_domain.get_grid();
+      remove_ghost(cells);
+
+			if(rank==0)
 			{
-				for(size_t i=0;i<particle_types->size();i++)
+				compute_domain_bounds(*domain,*bounds_mode,*enlarge_bounds, b, b, *pbc_adjust_xform );
+			}
+
+			//send bounds and size_box values to all cores
+			MPI_Bcast( & (*domain), sizeof(Domain), MPI_CHARACTER, 0, *mpi );
+			assert( check_domain(*domain) );
+			grid->set_offset( IJK{0,0,0} );
+			grid->set_origin( domain->bounds().bmin );
+			grid->set_cell_size( domain->cell_size() );
+			grid->set_dimension( domain->grid_dimension() );
+
+			// add particles
+			std::vector<ParticleTupleIO> particle_data;
+			ParticleTupleIO pt;
+			for (int c = 0 ; c < cells.get_size() ; c++ )
+			{
+				auto& cell = cells.get_data(c);
+				for (size_t s = 0 ; s < cell.get_size() ; s++)
 				{
-					typeMap[particle_types->at(i).m_name]=i;
+					Vec3d pos;
+					pos.x = cell.get_center(s, 0);
+					pos.y = cell.get_center(s, 1);
+					pos.z = cell.get_center(s, 2);
+					auto id = cell.get_id(s);
+					pt = ParticleTupleIO( pos.x, pos.y, pos.z, id, *type );
+					particle_data.push_back(pt);
 				}
-				nextTypeId = particle_types->size();
 			}
 
 
+			// Fill grid, particles will migrate accross mpi processed via the operator migrate_cell_particles
+			for( auto p : particle_data )
+			{
+				Vec3d r{ p[field::rx] , p[field::ry] , p[field::rz] };
+				IJK loc = domain_periodic_location( *domain , r ); //grid.locate_cell(r);
+				assert( grid->contains(loc) );
+				assert( min_distance2_between( r, grid->cell_bounds(loc) ) < grid->epsilon_cell_size2() );
+				p[field::rx] = r.x;
+				p[field::ry] = r.y;
+				p[field::rz] = r.z;
+				ParticleTuple t = p;
+				grid->cell( loc ).push_back( t , grid->cell_allocator() );
+			}
 
-      if( *init_domain )
-      {    
-        const double box_size_x = repeats->i * size.x;
-        const double box_size_y = repeats->j * size.y;
-        const double box_size_z = repeats->k * size.z;
-        domain_size = Vec3d{ box_size_x, box_size_y, box_size_z };
-
-        if(rank==0)
-        {
-	        AABB lattice_bounds  = { { 0., 0., 0. } , {box_size_x,box_size_y,box_size_z} };
-	        ldbg << "Lattice bounds      = "<<lattice_bounds<<std::endl;
-
-	        AABB computed_bounds = lattice_bounds;
-	        ldbg << "Computed_bounds  = " << computed_bounds << std::endl;
-
-          compute_domain_bounds(*domain,*bounds_mode,*enlarge_bounds,lattice_bounds,computed_bounds, *pbc_adjust_xform );
-        }
-        
-        //send bounds and size_box values to all cores
-        MPI_Bcast( & (*domain), sizeof(Domain), MPI_CHARACTER, 0, *mpi );
-        assert( check_domain(*domain) );
-
-        // compute local processor's grid size and location so that cells are evenly distributed
-        GridBlock in_block = { IJK{0,0,0} , domain->grid_dimension() };
-        GridBlock out_block = simple_block_rcb( in_block, np, rank );
-        ldbg<<"Domain = "<< *domain << std::endl;
-        ldbg<<"In  block = "<< in_block << std::endl;
-        ldbg<<"Out block = "<< out_block << std::endl;
-
-        // initializes local processor's grid
-        grid->set_offset( out_block.start );
-        grid->set_origin( domain->bounds().bmin );
-        grid->set_cell_size( domain->cell_size() );
-        local_grid_dim = out_block.end - out_block.start;
-        grid->set_dimension( local_grid_dim );      
-      }
-
-
-
-
-			std::vector<ParticleTupleIO> particle_data;
-
-
-			// file particle data
-			particle_data.push_back( ParticleTupleIO(x,y,z,n_particles++,typeMap[type]) );
-
-			ldbg << "min position xyz file : " << min_x << " " << min_y << " " << min_z << std::endl;
-			ldbg << "max position xyz file : " << max_x << " " << max_y << " " << max_z << std::endl;
-
-			//DOMAIN
-			AABB computed_bounds = { {min_x, min_y, min_z} , {max_x, max_y, max_z} };
-			ldbg << "computed_bounds  = " << computed_bounds << std::endl;
-
-			//domain->m_bounds = bounds;
-			compute_domain_bounds(*domain,*bounds_mode,*enlarge_bounds,file_bounds,computed_bounds, false );
-
+			// Display information
+			lout << "=================================" << std::endl;
 			lout << "Particles        = "<<particle_data.size()<<std::endl;
 			lout << "Domain XForm     = "<<domain->xform()<<std::endl;
 			lout << "Domain bounds    = "<<domain->bounds()<<std::endl;
@@ -143,40 +151,9 @@ using namespace exanb;
 			lout << "Real size        = "<<bounds_size(domain->bounds()) * Vec3d{domain->xform().m11,domain->xform().m22,domain->xform().m33} <<std::endl;
 			lout << "Cell size        = "<<domain->cell_size()<<std::endl;
 			lout << "Grid dimensions  = "<<domain->grid_dimension()<<" ("<<grid_cell_count(domain->grid_dimension())<<" cells)"<< std::endl;
-
-			//send bounds and size_box values to all cores
-			MPI_Bcast( & (*domain), sizeof(Domain), MPI_CHARACTER, 0, *mpi);
-			assert( check_domain(*domain) );
-
-			grid->set_offset( IJK{0,0,0} );
-			grid->set_origin( domain->bounds().bmin );
-			grid->set_cell_size( domain->cell_size() );
-			grid->set_dimension( domain->grid_dimension() );
-
-			if(rank==0)
-			{
-				for( auto p : particle_data )
-				{
-					Vec3d r{ p[field::rx] , p[field::ry] , p[field::rz] };
-					IJK loc = domain_periodic_location( *domain , r ); //grid.locate_cell(r);
-					assert( grid->contains(loc) );
-					assert( min_distance2_between( r, grid->cell_bounds(loc) ) < grid->epsilon_cell_size2() );
-					p[field::rx] = r.x;
-					p[field::ry] = r.y;
-					p[field::rz] = r.z;
-					ParticleTuple t = p;
-					grid->cell( loc ).push_back( t , grid->cell_allocator() );
-				}
-			}
-
-			lout << "============================" << std::endl;
+			lout << "=================================" << std::endl;
 
 			grid->rebuild_particle_offsets();
-
-#     ifndef NDEBUG
-			bool particles_inside_cell = check_particles_inside_cell(*grid);
-			assert( particles_inside_cell );
-#     endif
 		}
 
 	};
