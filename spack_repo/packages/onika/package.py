431e322be6f3cfbae092eebbb54c14931d677969
rprat-pro <rp269144@pleiades1217.intra.cea.fr>
2025-03-09 12:14:59 +0100

    update spack installation

---
+++ spack_repo/packages/onika/package.py
@@ -0,0 +1,30 @@
+from spack import *
+
+class Onika(CMakePackage):
+    """Onika is runtime library.
+    """
+
+    homepage = "https://github.com/Collab4exaNBody/onika"
+    git = "https://github.com/Collab4exaNBody/onika.git"
+
+
+    version("main",  git='https://github.com/Collab4exaNBody/onika.git', branch='main')
+    version("exadem-exanbody-2.0",  git='https://github.com/Collab4exaNBody/onika.git', tag='exadem-exanbody-2.0')
+
+    depends_on("cmake")
+    variant("cuda", default=False, description="Support for GPU")
+    depends_on("yaml-cpp")
+    depends_on("cuda", when="+cuda")
+    default_build_system = "cmake"
+    build_system("cmake", default="cmake")
+
+    variant(
+        "build_type",
+        default="Release", 
+        values=("Release", "Debug", "RelWithDebInfo"),
+        description="CMake build type",
+        )
+
+    def cmake_args(self):
+      args = [ self.define_from_variant("-DONIKA_BUILD_CUDA=ON", "cuda"), ]
+      return args
