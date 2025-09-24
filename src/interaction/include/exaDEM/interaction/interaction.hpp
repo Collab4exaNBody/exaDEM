25bece3d713cfe4223ca7ae0691e32c50405f109
Raphaël Prat <60797514+rprat-pro@users.noreply.github.com>
2025-09-24 14:47:37 +0200

    avoid interaction doublons in ghost area when compressing data

--- src/interaction/include/exaDEM/interaction/interaction.hpp
+++ src/interaction/include/exaDEM/interaction/interaction.hpp
@@ -63,7 +63,7 @@ namespace exaDEM
     ONIKA_HOST_DEVICE_FUNC bool is_active() const
     {
       constexpr exanb::Vec3d null = {0, 0, 0};
-      bool res = (moment != null) || (friction != null);
+      bool res = (moment != null) || (friction != null) && id_i < id_j;
       return res;
     }
 
