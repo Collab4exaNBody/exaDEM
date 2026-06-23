fec2171048d2012ce215c542ac8fd48234a49ae4
Raphaël Prat <60797514+rprat-pro@users.noreply.github.com>
2026-06-23 12:29:15 +0200

    remove shape_printer file (#340)
    Co-authored-by: rprat-pro <rp269144@pleiades1217.intra.cea.fr>

--- src/io/write_paraview_obb_particles.cpp
+++ src/io/write_paraview_obb_particles.cpp
@@ -16,30 +16,198 @@ KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.
 */
-#include <vector>
-#include <iomanip>
-#include <cstdlib>
-#include <filesystem>
-#include <fstream>
-#include <iostream>
-#include <onika/scg/operator.h>
-#include <onika/scg/operator_slot.h>
-#include <onika/scg/operator_factory.h>
-#include <exanb/core/make_grid_variant_operator.h>
-#include <exanb/core/parallel_grid_algorithm.h>
-#include <exanb/core/grid.h>
-#include <exanb/core/domain.h>
+
+#include <mpi.h>
+
+// onika
 #include <onika/math/basic_types.h>
 #include <onika/math/basic_types_operators.h>
 #include <onika/math/basic_types_stream.h>
+#include <onika/scg/operator.h>
+#include <onika/scg/operator_factory.h>
+#include <onika/scg/operator_slot.h>
 #include <onika/string_utils.h>
-#include <exanb/compute/compute_pair_optional_args.h>
 
-#include <mpi.h>
+#include <cstdlib>
+
+// exanb
+
+#include <exanb/compute/compute_pair_optional_args.h>
+#include <exanb/core/domain.h>
+#include <exanb/core/grid.h>
+#include <exanb/core/make_grid_variant_operator.h>
+#include <exanb/core/parallel_grid_algorithm.h>
+
+// exaDEM
 #include <exaDEM/shapes.hpp>
-#include <exaDEM/shape_printer.hpp>
+#include <filesystem>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <vector>
 
 namespace exaDEM {
+
+struct par_obb_helper {
+  int count = 0;  // use to count the number of corners
+  int off = 0;    // use for lines
+  std::stringstream corners;
+  std::stringstream lines;
+  std::stringstream offsets;
+  std::stringstream ids;
+  std::stringstream types;
+};
+
+inline void build_buffer_obb(const exanb::Vec3d& pos, const uint64_t id, const uint16_t type, const shape* shp,
+                             const exanb::Quaternion& orient, par_obb_helper& buffers) {
+  // add [1] corner [2] lines [3] id [4] type
+
+  double homothety = 1;
+  vec3r p = conv_to_vec3r(pos);
+  OBB obbi = shp->obb;
+  quat Q = conv_to_quat(orient);
+  obbi.rotate(Q);
+  obbi.extent *= homothety;
+  obbi.center *= homothety;
+  obbi.center += p;
+  vec3r corner;
+
+  auto add_corner = [&buffers](vec3r& c) { buffers.corners << " " << c[0] << " " << c[1] << " " << c[2]; };
+  auto add_offset = [&buffers](int incr) {
+    buffers.offsets << " " << buffers.off;
+    buffers.off += incr;
+  };
+
+  // [1] Add Corners
+
+  vec3r e0 = obbi.e1;  //
+  vec3r e1 = obbi.e2;  //
+  vec3r e2 = obbi.e3;  //
+
+  corner = obbi.center - obbi.extent[0] * e0 - obbi.extent[1] * e1 - obbi.extent[2] * e2;
+  add_corner(corner);
+  corner += 2.0 * obbi.extent[0] * e0;
+  add_corner(corner);
+  corner += 2.0 * obbi.extent[1] * e1;
+  add_corner(corner);
+  corner -= 2.0 * obbi.extent[0] * e0;
+  add_corner(corner);
+
+  corner = obbi.center - obbi.extent[0] * e0 - obbi.extent[1] * e1 - obbi.extent[2] * e2;
+  corner += 2.0 * obbi.extent[2] * e2;
+  add_corner(corner);
+  corner += 2.0 * obbi.extent[0] * e0;
+  add_corner(corner);
+  corner += 2.0 * obbi.extent[1] * e1;
+  add_corner(corner);
+  corner -= 2.0 * obbi.extent[0] * e0;
+  add_corner(corner);
+
+  // [2] add lines
+  int count = buffers.count;
+  add_offset(5);                 // 1
+  add_offset(5);                 // 2
+  add_offset(2);                 // 3
+  add_offset(2);                 // 4
+  add_offset(2);                 // 5
+  add_offset(2);                 // 6
+  buffers.lines << " " << count  // 1
+                << " " << count + 1 << " " << count + 2 << " " << count + 3 << " " << count;
+  buffers.lines << " " << count + 4  // 2
+                << " " << count + 5 << " " << count + 6 << " " << count + 7 << " " << count + 4;
+  buffers.lines << " " << count << " " << count + 4;      // 3
+  buffers.lines << " " << count + 1 << " " << count + 5;  // 4
+  buffers.lines << " " << count + 2 << " " << count + 6;  // 5
+  buffers.lines << " " << count + 3 << " " << count + 7;  // 6
+  buffers.count += 8;                                     // number of corners
+                                                          // [3] add id
+  for (int i = 0; i < 8; i++) {
+    buffers.ids << " " << id;
+  }
+
+  // [4] add type
+  for (int i = 0; i < 8; i++) {
+    buffers.types << " " << int(type);
+  }
+}
+
+inline void write_vtp_obb(std::string name, par_obb_helper& buffers) {
+  std::ofstream outFile(name);
+  if (!outFile) {
+    color_log::error("write_vtp_obb", "Impossible to open the file: " + name, false);
+    return;
+  }
+
+  int n_obb = buffers.count / 8;  // count = number of corners
+
+  outFile << "<?xml version=\"1.0\"?>" << std::endl;
+  outFile << "<VTKFile type=\"PolyData\" version=\"1.0\" header_type=\"UInt64\">" << std::endl;
+  outFile << "  <PolyData>" << std::endl;
+  outFile << "    <Piece NumberOfPoints=\"" << n_obb * 8 << "\" NumberOfLines=\"" << n_obb * 6 << "\" NumberOfPolys=\""
+          << 0 << "\">" << std::endl;
+  outFile << "    <PointData>" << std::endl;
+  outFile << "      <DataArray type=\"Int64\" Name=\"Id\"  NumberOfComponents=\"1\" format=\"ascii\">" << std::endl;
+  outFile << buffers.ids.rdbuf() << std::endl;
+  outFile << "      </DataArray>" << std::endl;
+  outFile << "      <DataArray type=\"Int32\" Name=\"Type\"  NumberOfComponents=\"1\" format=\"ascii\">" << std::endl;
+  outFile << buffers.types.rdbuf() << std::endl;
+  outFile << "      </DataArray>" << std::endl;
+  outFile << "    </PointData>" << std::endl;
+  outFile << "    <Points>" << std::endl;
+  outFile << "      <DataArray type=\"Float64\" Name=\"\"  NumberOfComponents=\"3\" format=\"ascii\">" << std::endl;
+  if (n_obb != 0) {
+    outFile << buffers.corners.rdbuf() << std::endl;
+  }
+  outFile << "      </DataArray>" << std::endl;
+  outFile << "    </Points>" << std::endl;
+  outFile << "    <Lines>" << std::endl;
+  outFile << "      <DataArray type=\"Int32\" Name=\"connectivity\" format=\"ascii\">" << std::endl;
+  if (n_obb != 0) {
+    outFile << buffers.lines.rdbuf() << std::endl;
+  }
+  outFile << "      </DataArray>" << std::endl;
+  outFile << "      <DataArray type=\"Int32\" Name=\"offsets\" format=\"ascii\">" << std::endl;
+  if (n_obb != 0) {
+    outFile << buffers.offsets.rdbuf() << std::endl;
+  }
+  outFile << "      </DataArray>" << std::endl;
+  outFile << "    </Lines>" << std::endl;
+  outFile << "    </Piece>" << std::endl;
+  outFile << "  </PolyData>" << std::endl;
+  outFile << "</VTKFile>" << std::endl;
+}
+
+inline void write_pvtp_obb(std::string basedir, std::string basename, size_t number_of_files) {
+  std::string name = basedir + "/" + basename + ".pvtp";
+  std::ofstream outFile(name);
+  if (!outFile) {
+    color_log::error("write_pvtp_obb", "Impossible to open the file: " + name, false);
+    return;
+  }
+
+  outFile << "<?xml version=\"1.0\"?>" << std::endl;
+  outFile << "<VTKFile type=\"PPolyData\"> " << std::endl;
+  outFile << "  <PPolyData GhostLevel=\"0\">" << std::endl;
+  outFile << "    <PPointData>" << std::endl;
+  outFile << "      <PDataArray type=\"Int64\" Name=\"Id\"  NumberOfComponents=\"1\"/>" << std::endl;
+  outFile << "      <PDataArray type=\"Int32\" Name=\"Type\"  NumberOfComponents=\"1\"/>" << std::endl;
+  outFile << "    </PPointData>" << std::endl;
+  outFile << "    <PPoints>" << std::endl;
+  outFile << "      <PDataArray type=\"Float64\" NumberOfComponents=\"3\"/>" << std::endl;
+  outFile << "    </PPoints> " << std::endl;
+  outFile << "    <PLines>" << std::endl;
+  outFile << "      <PDataArray type=\"Int32\" Name=\"connectivity\"  NumberOfComponents=\"1\"/>" << std::endl;
+  outFile << "      <PDataArray type=\"Int32\" Name=\"offsets\"  NumberOfComponents=\"1\"/>" << std::endl;
+  outFile << "    </PLines> " << std::endl;
+  std::string subfile = basename + "/%06d.vtp";
+  for (size_t i = 0; i < number_of_files; i++) {
+    std::string file = onika::format_string(subfile, i);
+    outFile << "    <Piece Source=\"" << file << "\"/>" << std::endl;
+  }
+  outFile << "  </PPolyData>" << std::endl;
+  outFile << "</VTKFile>" << std::endl;
+}
+
 template <class GridT, class = AssertGridHasFields<GridT>>
 class WriteParaviewOBBParticlesOperator : public OperatorNode {
   using ComputeFields = FieldSet<field::_rx, field::_ry, field::_rz, field::_type, field::_orient>;
@@ -122,7 +290,7 @@ class WriteParaviewOBBParticlesOperator : public OperatorNode {
 
 // === register factories ===
 ONIKA_AUTORUN_INIT(write_paraview_obb_particles) {
-  OperatorNodeFactory::instance()->register_factory(
-      "write_paraview_obb_particles", make_grid_variant_operator<WriteParaviewOBBParticlesOperator>);
+  OperatorNodeFactory::instance()->register_factory("write_paraview_obb_particles",
+                                                    make_grid_variant_operator<WriteParaviewOBBParticlesOperator>);
 }
 }  // namespace exaDEM
