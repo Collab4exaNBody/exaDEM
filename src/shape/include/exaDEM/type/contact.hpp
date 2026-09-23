c74aaecf5eb2c3f22b8ba84e50cd975cb1ee0101
rprat-pro <rp269144@pleiades1217.intra.cea.fr>
2026-09-23 13:55:15 +0200

    fix message for contact, negative = penetration

--- src/shape/include/exaDEM/type/contact.hpp
+++ src/shape/include/exaDEM/type/contact.hpp
@@ -3,7 +3,7 @@
 namespace exaDEM {
 struct contact {
   bool is_contact = false;  // true if contact is detected
-  double dn = 0;            // normal overlap (positive if penetration, negative if separation)
+  double dn = 0;            // normal gap (distance - sum of radii): negative if penetration, positive if separation
   exanb::Vec3d normal;      // contact normal
   exanb::Vec3d position;    // contact position
 };
